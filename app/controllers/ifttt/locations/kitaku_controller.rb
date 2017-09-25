0cde107b6fd89ee547ed3ae44c314ce723f5f35d
Kohei Onodera <onodera212@gmail.com>
2017-09-26 07:07:59 +0900

    modified timezone

--- app/controllers/ifttt/locations/kitaku_controller.rb
+++ app/controllers/ifttt/locations/kitaku_controller.rb
@@ -2,18 +2,21 @@ class Ifttt::Locations::KitakuController < ApplicationController
   include SlackClient
 
   def create
-    if can_deliver_message?
-      message = "#{ Rails.application.secrets.slack[:kitaku_message] }\nI will be arriving at Home around #{ (Time.now + 30.minutes).strftime("%H:%M") }."
+    time_now = Time.zone.now.in_time_zone('Asia/Tokyo')
+
+    if can_deliver_message?(time_now)
+      message = "#{ Rails.application.secrets.slack[:kitaku_message] }\nI will be arriving at Home around #{ (time_now + 30.minutes).strftime("%H:%M") }."
       slack_push(message: message, username: "Kitaku Bot", icon_emoji: ":train:")
     end
+
     render json: { status: :success }
   end
 
   private
 
-  def can_deliver_message?
+  def can_deliver_message?(time_now)
     # between 17:00 and 23:59
-    hour = Time.now.hour
+    hour = time_now.hour
     (hour >= 17 && hour <= 23)
   end
 end
